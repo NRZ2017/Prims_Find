36df5f25daa42bc689d9eaae6d19d75724a44e9e
Nick Zhao <nickzrz@hotmail.com>
2019-09-15 14:32:13 -0700

    Need to finish the setup

---
+++ Prims_Find/Prims_Find/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prims_Find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
